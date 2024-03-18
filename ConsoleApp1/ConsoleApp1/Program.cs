e1c15bb796a1513acb241cc7a5672daad359795d
Ataberkan <ataberkangazi132@gmail.com>
2024-03-18 22:55:14 +0100

    Implemented CalculateAverage method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,16 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
+
+public static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        throw new ArgumentException("Array cannot be null or empty.", nameof(numbers));
+    
+    double sum = 0;
+    foreach (int num in numbers)
+    {
+        sum += num;
+    }
+    return sum / numbers.Length;
+}
